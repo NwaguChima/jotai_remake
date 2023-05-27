2d8ed1e2120a1c7b77aae57dea295f38e662cdd1
NwaguChima <nwaguchima48@gmail.com>
2023-05-27 10:52:47 +0100

    added getters and setters

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { atom, useAtom } from './jotai';
+
+const salaryAtom = atom<number>(100_000);
 
 function App() {
-  return <div></div>;
+  const [salary, setSalary] = useAtom(salaryAtom);
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={salary as number}
+          // @ts-ignore
+          onChange={(e) => setSalary(Number(e.target.value))}
+        />
+      </div>
+
+      <div>
+        Salary:{' '}
+        {new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        }).format(salary as number)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/jotai.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+interface Atom<AtomType> {
+  get: () => AtomType;
+  set: (newValue: AtomType) => void;
+  subscribe: (callback: (newValue: AtomType) => void) => () => void;
+}
+
+export function atom<AtomType>(initialValue: AtomType): Atom<AtomType> {
+  let value = initialValue;
+
+  const subscribers = new Set<(newValue: AtomType) => void>();
+
+  return {
+    get: () => value,
+    set: (newValue) => {
+      value = newValue;
+      subscribers.forEach((callback) => callback(value));
+    },
+    subscribe: (callback) => {
+      subscribers.add(callback);
+
+      return () => {
+        subscribers.delete(callback);
+      };
+    },
+  };
+}
+
+export function useAtom<AtomType>(atom: Atom<AtomType>) {
+  const [value, setValue] = useState(atom.get());
+
+  useEffect(() => {
+    const unsubscribe = atom.subscribe(setValue);
+
+    return () => {
+      unsubscribe();
+    };
+  }, [atom]);
+
+  return [value, atom.set];
+}
